21fceb70598c6fa3b3b3bc14f0f43a000dc509a2
gu1mira <sidagulova@gmail.com>
2024-02-05 20:32:33 +0200

    task3

--- GitDenemeClass.java
+++ GitDenemeClass.java
@@ -3,11 +3,14 @@ package GitDeneme1;
 public class GitDenemeClass {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Merhaba 3");
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Menin atym");
+        }
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Merhaba 5");
 
         }
+        System.out.println("Merhaba alt 4");
     }
 }
