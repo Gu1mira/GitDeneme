e06f58653d1475f8e25620b380f7344782104493
gu1mira <sidagulova@gmail.com>
2024-02-05 19:35:05 +0200

    Kredi karti eklendi

--- GitDenemeClass.java
+++ GitDenemeClass.java
@@ -3,5 +3,6 @@ package GitDeneme1;
 public class GitDenemeClass {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Merhaba 3");
     }
 }
