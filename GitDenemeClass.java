e5edc8651a2d0ac8d558a2ebec293828ed6633dd
gu1mira <sidagulova@gmail.com>
2024-02-05 19:39:07 +0200

    Satin almak islemi eklendi

--- GitDenemeClass.java
+++ GitDenemeClass.java
@@ -4,5 +4,10 @@ public class GitDenemeClass {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Merhaba 3");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Merhaba 5");
+
+        }
     }
 }
